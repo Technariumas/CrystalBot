a7ea3737b4b045591d2e14fb86c5aadb4586191d
opit <opit@hardcore.lt>
2018-02-11 17:36:19 +0200

    comments

--- draw.py
+++ draw.py
@@ -1,50 +1,55 @@
-from __future__ import division
+#!/usr/bin/python3
+
 import sys
 import MySQLdb
 import wget
 import os 
 import subprocess
 
+#Access MySQL via Python..
 db = MySQLdb.connect(host="sql.crystallography.net",    
                      user="cod_reader", db="cod")        
 
 cur = db.cursor()
-#select the latest added structure
-#cur.execute('select file, formula, date, time from data where flags like "%has Fobs%" order by date desc, time  DESC  limit 1')
+#select the latest added structure from COD
+cur.execute('select file, formula, date, time from data where flags like "%has Fobs%" order by date desc, time  DESC  limit 1')
 
 #select random structure
-cur.execute('select file, formula, date, time from data where flags like "%has Fobs%" order by rand() limit 1')
-
+#cur.execute('select file, formula, date, time from data where flags like "%has Fobs%" order by rand() limit 1')
 
 for row in cur.fetchall():
     cifId = str(row[0])
     formula = str(row[1])
-    date = str(row[2])    
-    time = str(row[3])
-    print date, time
 db.close()
 
 cifFile = "http://www.crystallography.net/cod/"+cifId+".cif"
 
+#Downloads the CIF file
 wget.download(cifFile, "cif/"+cifId+".cif")
 #os.system("jmol cif/"+cifId+".cif")
 
+#Creates Jmol script that generates an annotated 2D molecule snapshot 
 file = open("scripts/"+cifId+".spt","w") 
 file.write("load cif/"+cifId+".cif;\n")
-#imgFilename = cifId+".png"
-#file.write("select all;")
-#file.write("label %a;")
 file.write("set echo example 0% 5%;\n")
 file.write("font echo 63 serif bold;\n") 
 file.write("echo " +formula+ " ("+cifId+");\n" )
-
 file.write('write IMAGE 842 595 PNG n "png";') 
 file.close() 
-os.system("jmol scripts/"+cifId+".spt &&")
-os.system("convert png -negate  -threshold 80% output.bmp")
-os.system("potrace --svg output.bmp -t 10 -a 3 -u 10 -P A4 --group -o vect.svg")
 
-# call the plotter:
+#Runs Jmol script
+os.system("jmol scripts/"+cifId+".spt &&")
+
+#Runs ImageMagick's convert utility to invert image colours and convert it to black and white
+os.system("convert png -negate  -threshold 80% output.bmp")
+
+#Runs potrace to vectorize the generated bmp file
+os.system("potrace --svg output.bmp -t 10 -a 3 -u 10 -P A4 --group -o vect-grib.svg")
+
+#This is a shell script that corrects the output svg measurements -- thanks to ffwd!
+os.system("./grib.sh")
+
+# calls the plotter:
 subprocess.check_output(['axibot', 'plot', 'vect.svg'])
 
 
