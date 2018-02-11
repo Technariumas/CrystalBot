6965159c913c10a29dda7dc1313aa52ea664a953
opit <opit@hardcore.lt>
2018-02-11 15:55:14 +0200

    latest or random entry

--- draw.py
+++ draw.py
@@ -4,20 +4,24 @@ import MySQLdb
 import wget
 import os 
 import subprocess
-import pysvg.structure
-import pysvg.builders
-import pysvg.text
 
 db = MySQLdb.connect(host="sql.crystallography.net",    
                      user="cod_reader", db="cod")        
 
 cur = db.cursor()
+#select the latest added structure
+#cur.execute('select file, formula, date, time from data where flags like "%has Fobs%" order by date desc, time  DESC  limit 1')
 
-cur.execute('select file, formula from data where flags like "%has Fobs%" order by rand() limit 1')
+#select random structure
+cur.execute('select file, formula, date, time from data where flags like "%has Fobs%" order by rand() limit 1')
+
 
 for row in cur.fetchall():
     cifId = str(row[0])
     formula = str(row[1])
+    date = str(row[2])    
+    time = str(row[3])
+    print date, time
 db.close()
 
 cifFile = "http://www.crystallography.net/cod/"+cifId+".cif"
@@ -34,17 +38,11 @@ file.write("set echo example 0% 5%;\n")
 file.write("font echo 63 serif bold;\n") 
 file.write("echo " +formula+ " ("+cifId+");\n" )
 
-file.write('write IMAGE 2000 1600 PNG n "png";') 
+file.write('write IMAGE 842 595 PNG n "png";') 
 file.close() 
-os.system("jmol scripts/"+cifId+".spt")
+os.system("jmol scripts/"+cifId+".spt &&")
 os.system("convert png -negate  -threshold 80% output.bmp")
-os.system("potrace --svg output.bmp -t 10 -a 3 -u 10 -o vect.svg")
-
-
-svg_doc = pysvg.structure.Svg(height="210mm", width="297mm")
-svg_doc.set_viewBox("0 0 1052.3622047 744.09448819")
-
-style_builder = pysvg.builders.StyleBuilder()
+os.system("potrace --svg output.bmp -t 10 -a 3 -u 10 -P A4 --group -o vect.svg")
 
 # call the plotter:
 subprocess.check_output(['axibot', 'plot', 'vect.svg'])
